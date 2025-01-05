05f8424b39a8b3cef36554dbe341cea1f569ec96
AdeemAmir <adeemamir21@gmail.com>
2025-01-06 00:59:33 +0500

    Submodule Fix

--- scripts/weathCall.js
+++ scripts/weathCall.js
@@ -1,8 +1,8 @@
 import { xpCol } from '../CommonJS/manage.mjs';
 
 let apiW = { key: '', base: "https://api.openweathermap.org/data/2.5/" };
-xpCol().then(x => {apiW.key = x;});
-
+xpCol(1).then(x => {apiW.key = x;});
+console.log(apiW)
 /**
  * Get weather information for a given city and country.
  * @param {string} city - City name.
